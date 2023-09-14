85d265e326444eb3358691fc1b1a7f1f1b444fd7
nguyenvanhuy0612 <nguyenvanhuy0612@gmail.com>
2023-09-14 19:44:27 +0700

    update

---
+++ src/test/java/com.huy.test/TestCase1.java
@@ -0,0 +1,15 @@
+package com.huy.test;
+
+import org.junit.Test;
+
+public class TestCase1 {
+    @Test
+    public void test1() {
+        System.out.println("Test1");
+    }
+
+    @Test
+    public void test2() {
+        System.out.println("Test2");
+    }
+}
